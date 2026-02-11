95fd0e1e681be165c19068b2ba2efb3deb807519
Sumit <sumitsnghtmr@gmail.com>
2026-02-11 17:44:54 +0530

    Add routes & middleware

---
+++ routes.ts
@@ -0,0 +1,8 @@
+export const publicRoutes: string[] = [];
+
+export const privateRoutes: string[] = ["/"];
+
+export const authRoutes: string[] = ["auth/sign-in"];
+
+export const apiAuthPrefix: string = "/api/auth";
+export const DEFAULT_LOGIN_REDIRECT = "/";
