def650150b020424c430175d27775500f75a0a13
Sumit <sumitsnghtmr@gmail.com>
2026-02-21 21:01:39 +0530

    Implement home page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from "next";
+import { Header } from "@/modules/home/header";
+import { Footer } from "@/modules/home/footer";
+import { cn } from "@/lib/utils";
+
+export const metadata: Metadata = {
+    title: {
+        template: "VibeCode - Editor",
+        default: "Code Editor For VibeCoders - VibeCode",
+    },
+};
+
+export default function HomeLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="relative min-h-screen overflow-hidden">
+            <Header />
+            <div
+                className={cn(
+                    "absolute inset-0",
+                    "bg-size-[40px_40px]",
+                    "bg-[linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+                    "dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+                )}
+            />
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+            <main className="z-20 realtive w-full pt-0">{children}</main>
+            <Footer />
+        </div>
+    );
+}
