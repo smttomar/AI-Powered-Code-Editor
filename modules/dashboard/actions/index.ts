755b2ef4461336c1c4362c3909e094f0e0ef1058
Sumit <sumitsnghtmr@gmail.com>
2026-02-23 06:57:57 +0530

    Server actions Related to Dashboard

--- modules/dashboard/actions/index.ts
+++ modules/dashboard/actions/index.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/lib/db";
 import { currentUser } from "@/modules/auth/actions";
+import { revalidatePath } from "next/cache";
 
 export const getAllPlaygroundForUser = async () => {
     const user = await currentUser();
@@ -17,6 +18,89 @@ export const getAllPlaygroundForUser = async () => {
         });
 
         return playground;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const createPlayground = async (data: {
+    title: string;
+    template: "REACT" | "NEXTJS" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR";
+    description?: string;
+}) => {
+    const user = await currentUser();
+
+    const { template, title, description } = data;
+
+    try {
+        const playground = await db.playground.create({
+            data: {
+                title: title,
+                description: description,
+                template: template,
+                userId: user?.id!,
+            },
+        });
+        return playground;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteProjectById = async (id: string) => {
+    try {
+        await db.playground.delete({
+            where: {
+                id,
+            },
+        });
+        revalidatePath("/");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const editProjectById = async (
+    id: string,
+    data: { title: string; description: string },
+) => {
+    try {
+        await db.playground.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+        revalidatePath("/dashboard");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const duplicateProjectById = async (id: string) => {
+    try {
+        const originalPlayground = await db.playground.findUnique({
+            where: {
+                id,
+            },
+            // add template file
+        });
+        if (!originalPlayground) {
+            throw new Error("Original playground not found");
+        }
+
+        const duplicatedPlayground = await db.playground.create({
+            data: {
+                title: `${originalPlayground.title} (Copy)`,
+                description: originalPlayground.description,
+                template: originalPlayground.template,
+                userId: originalPlayground.userId,
+
+                // template file
+            },
+        });
+        revalidatePath("/dashboard");
+        return duplicatedPlayground;
     } catch (error) {
         console.log(error);
     }
