f1eb8b0afa789a16f2ff1a2b25545875acab0343
Sumit <sumitsnghtmr@gmail.com>
2026-03-21 20:35:22 +0530

    Monaco Editor part 1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -3,26 +3,51 @@ import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { usePlayground } from "@/modules/playground/hooks/usePlayground";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { Separator } from "@/components/ui/separator";
 import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
-
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
+import { TemplateFile } from "@/modules/playground/lib/path-to-json";
 const MainPlaygroundPage = () => {
     const { id } = useParams<{ id: string }>();
 
     const { playgroundData, templateData, isLoading, error, saveTemplateData } =
         usePlayground(id);
 
-    console.log("templateData", templateData);
-    console.log("playgroundData", playgroundData);
-    const activeFile = "Sample.txt";
+    const {
+        activeFileId,
+        closeAllFiles,
+        openFile,
+        openFiles,
+        setTemplateData,
+        setActiveFileId,
+        setPlaygroundId,
+        setOpenFiles,
+    } = useFileExplorer();
+
+    useEffect(() => {
+        setPlaygroundId(id);
+    }, [id, setPlaygroundId]);
+
+    useEffect(() => {
+        if (templateData && !openFiles.length) {
+            setTemplateData(templateData);
+        }
+    }, [templateData, setTemplateData, openFiles.length]);
+
+    const activeFile = openFiles.find((file) => file.id === activeFileId);
+    const hasUnsavedChnages = openFiles.some((file) => file.hasUnsavedChanges);
+
+    const handleFileSelect = (file: TemplateFile) => {
+        openFile(file);
+    };
 
     return (
         <TooltipProvider>
             <>
                 <TemplateFileTree
                     data={templateData!}
-                    onFileSelect={() => {}}
+                    onFileSelect={handleFileSelect}
                     selectedFile={activeFile}
                     title="File Explorer"
                     onAddFile={() => {}}
