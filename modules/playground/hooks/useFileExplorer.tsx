5203dd8a48fd6efa48f914f0920dc743a5ba4e24
Sumit <sumitsnghtmr@gmail.com>
2026-03-21 18:46:48 +0530

    Monaco Editor part 1

---
+++ modules/playground/hooks/useFileExplorer.tsx
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { toast } from "sonner";
+
+import { TemplateFile, TemplateFolder } from "../lib/path-to-json";
+
+interface OpenFile extends TemplateFile {
+    id: string;
+    hasUnsavedChanges: boolean;
+    content: string;
+    originalContent: string;
+}
+
+interface FileExplorerState {
+    playgroundId: string;
+    templateData: TemplateFolder | null;
+    openFiles: OpenFile[];
+    activeFileId: string | null;
+    editorContent: string;
+
+    setPlaygroundId: (id: string) => void;
+    setTemplateData: (data: TemplateFolder | null) => void;
+    setEditorContent: (content: string) => void;
+    setOpenFiles: (files: OpenFile[]) => void;
+    setActiveFileId: (fileId: string | null) => void;
+}
