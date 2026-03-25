8c1767b3e566fbbf74163e39ad1b5a640e780ca1
Sumit <sumitsnghtmr@gmail.com>
2026-03-25 13:13:29 +0530

    Update config file

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,13 @@ const nextConfig: NextConfig = {
             },
         ];
     },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /codefile-starters/,
+            use: "ignore-loader",
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
