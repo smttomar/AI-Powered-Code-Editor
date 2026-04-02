216935a325628302f043e947f0ab47ace074bb1a
Sumit <sumitsnghtmr@gmail.com>
2026-04-02 17:22:59 +0530

    UI chnages sign in page

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className="flex justify-center items-center h-screen flex-col bg-zinc-800">
+        <main className="flex justify-center items-center h-screen flex-col dark:bg-zinc-800 bg-zinc-200">
             {children}
         </main>
     );
